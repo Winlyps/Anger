7167393d1bd2f4e54fa8bc8aaa4dd20954a8cfa8
Winlypse <nts3@o2.pl>
2024-10-22 23:44:37 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/anger/Anger.kt
@@ -0,0 +1,14 @@
+package winlyps.anger
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Anger : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
